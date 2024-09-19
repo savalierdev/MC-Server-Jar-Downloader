357fc870ae5ca206f6baab5fed7592ad9aefbd3d
Alper Elieyi <alper.elieyi23@ogr.atauni.edu.tr>
2024-09-19 21:08:53 +0300

    Added some functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+import requests
+
+class Service:
+    def __init__(self, name):
+        pass
+
+
+class FileOperations:
+    def __init__(self):
+        pass
+
+    def create_server_folder(self, path):
+        pass
